dec9e22ee395cfa4b4c3d37088c5077d2ec18612
Jesús Paredes <jjesus.paredesl@gmail.com>
2022-10-04 03:24:35 -0300

    4-10

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var icon = document.getElementById("icon");
 
-icon.onclick = function () {
-    document.body.classList.toggle("dark-theme");
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "../Fotografía/Proyect img/sun.png"
-    } 
-    else {
-        icon.src = "../Fotografía/Proyect img/moon.png"
-    }
+
+const evento = document.getElementById('send')
+const enviarFormulario =() => {
+        let name = document.getElementById('name').value;
+        let surname = document.getElementById('surname').value;
+        let message = document.getElementById('message').value;
+        let number= 543512229161;
+var win= window.open(`https://wa.me/${number}?text=Hello!%20my%20name%20is%20${name}
+%20${surname}, I want to ...:%20${message}`,'_blank');       
 }
-
+evento.addEventListener('click', enviarFormulario)
